cfb22f917222ee855bafba8cca484850a53c6530
Dan Spelt <dans@neilsquire.ca>
2024-03-21 12:06:02 -0700

    Refactor imports and add type annotations to Home component

--- app/(front)/page.tsx
+++ app/(front)/page.tsx
@@ -1,25 +1,31 @@
 /* eslint-disable @next/next/no-img-element */
-import ProductItem from '@/components/products/ProductItem'
-import data from '@/lib/data'
-import productService from '@/lib/services/productService'
-import { convertDocToObj } from '@/lib/utils'
-import { Metadata } from 'next'
-import Link from 'next/link'
+import ProductItem from "@/components/products/ProductItem";
+import productService from "@/lib/services/productService";
+import { convertDocToObj } from "@/lib/utils";
+import { Metadata } from "next";
+import Link from "next/link";
 
 export const metadata: Metadata = {
-  title: process.env.NEXT_PUBLIC_APP_NAME || 'My Ecommerce Store',
+  title: process.env.NEXT_PUBLIC_APP_NAME || "My Ecommerce Store",
   description:
     process.env.NEXT_PUBLIC_APP_DESC ||
-    'Nextjs, Server components, Next auth, daisyui, zustand',
+    "Nextjs, Server components, Next auth, daisyui, zustand",
+};
+
+interface Product {
+  id: string;
+  slug: string;
+  banner: string;
+  name: string;
 }
 
 export default async function Home() {
-  const featuredProducts = await productService.getFeatured()
-  const latestProducts = await productService.getLatest()
+  const featuredProducts = await productService.getFeatured();
+  const latestProducts = await productService.getLatest();
   return (
     <>
       <div className="w-full carousel rounded-box mt-4">
-        {featuredProducts.map((product, index) => (
+        {featuredProducts.map((product: Product, index) => (
           <div
             key={product.id}
             id={`slide-${index}`}
@@ -55,10 +61,10 @@ export default async function Home() {
       </div>
       <h2 className="text-2xl py-2">Latest Products</h2>
       <div className="grid grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-4">
-        {latestProducts.map((product) => (
+        {latestProducts.map((product: Product) => (
           <ProductItem key={product.slug} product={convertDocToObj(product)} />
         ))}
       </div>
     </>
-  )
+  );
 }
